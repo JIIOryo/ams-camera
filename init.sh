d5da6f114a5da209a43a0232120dde762744438e
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-07-19 17:24:19 +0100

    [add] opencv dependencies

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
-# for debug
+# for gpio command
 sudo apt install wiringpi
+# install pip3
 sudo apt install python3-pip
 
+# install python package using pip3
 pip3 install -r requirements.txt
+
+# cv2 dependencies
+sudo apt install libatlas-base-dev libjasper-dev libqtgui4 python3-pyqt5 libqt4-test libilmbase-dev libopenexr-dev libgstreamer1.0-dev libavcodec58 libavformat58 libswscale5 -y
