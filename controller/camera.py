21c26cbdada2a4d4c9f867aede03cbfeebeef80f
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-07-24 05:57:13 +0100

    [add] trimming logic

--- controller/camera.py
+++ controller/camera.py
@@ -5,11 +5,23 @@ current_dir = pathlib.Path(__file__).resolve().parent
 sys.path.append( str(current_dir) + '/../' )
 
 from service.camera import take_picture
+from service.trimming import trimming
+
+tmp_picture_path = '/home/pi/ams-camera/tmp/now.jpg'
 
 def picture(request: dict) -> None:
 
     take_picture(
+        file = tmp_picture_path,
         x = request['resolution']['x'],
         y = request['resolution']['y'],
         warm_up_time = request['cameraWarmUpTime']
+    )
+
+    trimming(
+        file = tmp_picture_path,
+        top = request['trimming']['top'],
+        bottom = request['trimming']['bottom'],
+        left = request['trimming']['left'],
+        right = request['trimming']['right']
     )
--- service/camera.py
+++ service/camera.py
@@ -2,7 +2,7 @@ import time
 
 import picamera
 
-def take_picture(x: int, y: int, warm_up_time: int) -> None:
+def take_picture(file: str, x: int, y: int, warm_up_time: int) -> None:
     with picamera.PiCamera() as camera:
         camera.resolution = (x, y)
         camera.start_preview()
@@ -10,5 +10,5 @@ def take_picture(x: int, y: int, warm_up_time: int) -> None:
         # warm up
         time.sleep(warm_up_time)
 
-        camera.capture('/home/pi/ams-camera/tmp/now.jpg')
+        camera.capture(file)
     return
