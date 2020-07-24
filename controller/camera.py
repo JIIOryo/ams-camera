6729d22d98764803c606009b5cffdb272bc39126
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-07-24 14:11:28 +0100

    [refactor] TMP_PICTURE_PATH

--- controller/camera.py
+++ controller/camera.py
@@ -7,20 +7,21 @@ sys.path.append( str(current_dir) + '/../' )
 from service.camera import take_picture
 from service.trimming import trimming
 from service.uploader import s3_upload
+from lib.config import get_config_item
 
-tmp_picture_path = '/home/pi/ams-camera/tmp/now.jpg'
+TMP_PICTURE_PATH = get_config_item('tmp_picture_file_path')
 
 def picture(request: dict) -> None:
 
     take_picture(
-        file_ = tmp_picture_path,
+        file_ = TMP_PICTURE_PATH,
         x = request['resolution']['x'],
         y = request['resolution']['y'],
         warm_up_time = request['cameraWarmUpTime']
     )
 
     trimming(
-        file_ = tmp_picture_path,
+        file_ = TMP_PICTURE_PATH,
         top = request['trimming']['top'],
         bottom = request['trimming']['bottom'],
         left = request['trimming']['left'],
@@ -28,7 +29,7 @@ def picture(request: dict) -> None:
     )
 
     s3_upload(
-        file_ = tmp_picture_path,
+        file_ = TMP_PICTURE_PATH,
         object_name = request['objectName'],
         access_key_id = request['uploader']['aws']['accessKeyId'],
         secret_access_key = request['uploader']['aws']['secretAccessKey'],
