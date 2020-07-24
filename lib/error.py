f3729871cb39a1535a82dd73f4bdb57385b6287c
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-07-24 13:50:03 +0100

    [add] AmsCameraError and S3UploadError use this

--- lib/error.py
+++ lib/error.py
@@ -1,6 +1,9 @@
 
-class S3UploadError(Exception):
+class AmsCameraError(Exception):
     def __init__(self, error, message):
         self.error = error
         self.message = message
+    pass
+
+class S3UploadError(AmsCameraError):
     pass
