ad9574686c6e590112564622c82eb5c436e0da52
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-07-24 03:27:35 +0100

    [add] mkdir tmp dir

--- init.sh
+++ init.sh
@@ -8,3 +8,6 @@ pip3 install -r requirements.txt
 
 # cv2 dependencies
 sudo apt install libatlas-base-dev libjasper-dev libqtgui4 python3-pyqt5 libqt4-test libilmbase-dev libopenexr-dev libgstreamer1.0-dev libavcodec58 libavformat58 libswscale5 -y
+
+# tmp files
+mkdir tmp
