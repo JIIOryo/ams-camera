8e3fa6e0626e5cafd8ddb45b0b815bf9d6c83d0b
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-09-22 00:49:04 +0100

    [add] input file path and output file path

--- service/trimming.py
+++ service/trimming.py
@@ -1,7 +1,7 @@
 import cv2
 
-def trimming(file_: str, top: int, bottom: int, left: int, right: int) -> None:
-    img = cv2.imread(file_)
+def trimming(input_path: str, output_path: str, top: int, bottom: int, left: int, right: int) -> None:
+    img = cv2.imread(input_path)
     trimmed_img = img[top : bottom, left: right]
-    cv2.imwrite(file_, trimmed_img)
+    cv2.imwrite(output_path, trimmed_img)
     return
